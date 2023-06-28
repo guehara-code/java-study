b863ed885bfed3fe6291a7ecae4b6254c0ed558a
guehara-code <gil_uehara@yahoo.com.br>
2023-06-28 13:36:24 -0300

    first commit

---
+++ pg0027/Example.java
@@ -0,0 +1,12 @@
+/*
+    This is a simple Java program.
+    File must be called "Example.java"
+ */
+
+class Example {
+    // Program begins with call to main()
+    public static void main(String[] args) {
+        System.out.println("This is a simple Java program.");
+    }
+
+}
