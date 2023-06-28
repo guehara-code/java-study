e9925e30e956800c8ab6fbcffb66c6bc75adeb14
guehara-code <gil_uehara@yahoo.com.br>
2023-06-28 15:07:31 -0300

    more studies

---
+++ pg0042/Area.java
@@ -0,0 +1,13 @@
+// Compute the area of a circle
+
+class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+
+        r = 10.8; // radius of the circle
+        pi = 3.1416; // approximated pi number
+        a = pi * r * r;
+
+        System.out.println("Area of the circle is " + a);
+    }
+}
